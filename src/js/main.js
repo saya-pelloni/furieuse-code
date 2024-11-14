128c024a14db6a2aebfd0204da71f14b2882dbf8
saya-pelloni <122967529+saya-pelloni@users.noreply.github.com>
2024-11-14 09:11:00 +0100

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,39 @@
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+const viewportWidth = window.screen.width;
+const friseWidth = document.querySelector(".slider-element").offsetWidth;
+
+gsap.to(".slider-container", {
+  x: viewportWidth - friseWidth,
+  scrollTrigger: {
+    trigger: ".slider-mask",
+    start: "top top",
+    scrub: 1,
+    pin: true,
+  },
+});
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".flashback-baron-text", {
+  opacity: 10,
+  scrollTrigger: {
+    trigger: ".expression-baron",
+    start: "top center",
+    end: "bottom center",
+    toggleActions: "play reverse play reverse",
+    markers: false,
+  },
+});
+
+gsap.to(".flashback-baron-text-second", {
+  opacity: 10,
+  scrollTrigger: {
+    trigger: ".expression-baron-second",
+    start: "top center",
+    end: "bottom center",
+    toggleActions: "play reverse play reverse",
+    markers: false,
+  },
+});
